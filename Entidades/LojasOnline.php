4a5c8bd023ff61eb6a40f5b2835582033083cd0c
Juniorjobbs <junior.jobbins@gmail.com>
2024-04-07 22:47:43 -0300

    atualização entidade nova versao

--- Entidades/LojasOnline.php
+++ Entidades/LojasOnline.php
@@ -3,43 +3,51 @@
 /* V002/24 - versao 07-04-2024 */
 require_once "../Interfaces/LojasOnlineInterface.php";
 
-class LojasOnline {
+class LojasOnline implements LojasOnlineInterface {
     private $id;
     private $loja;
     private $url;
     private $disponibilidade;
 
+    // Adicionando injeção de dependências através do construtor
+    public function __construct($id, $loja, $url, $disponibilidade){
+        $this->id = $id;
+        $this->loja = $loja;
+        $this->url = $url;
+        $this->disponibilidade = $disponibilidade;
+ 
+    }    
 
     // Getters e Setters
-    public function SetId($id){
+    public function SetId(int $id): void {
         $this->id = $id;
     }
 
-    public function GetId(){
+    public function GetId(): int {
         return $this->id;
     }
 
-      public function SetLoja($loja){
+    public function SetLoja(string $loja): void {
         $this->loja = $loja;
     }
 
-    public function GetLoja(){
+    public function GetLoja(): string {
         return $this->loja;
     }
 
-      public function SetUrl($url){
+    public function SetUrl(string $url): void {
         $this->url = $url;
     }
 
-    public function GetUrl(){
+    public function GetUrl(): string {
         return $this->url;
     }
 
-    public function SetDisponibilidade($disponibilidade){
+    public function SetDisponibilidade(string $disponibilidade): void {
         $this->disponibilidade = $disponibilidade;
     }
 
-    public function GetDisponibilidade(){
+    public function GetDisponibilidade(): string {
         return $this->disponibilidade;
     }
    
